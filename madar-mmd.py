76c6f3bcace69528d333fc1f51675821bd766dd6
mamad-payambar <111741244+mamad-payambar@users.noreply.github.com>
2022-08-22 14:06:53 +0430

    Create madar-mmd.py

---
+++ madar-mmd.py
@@ -0,0 +1,27 @@
+import sys
+Y = "\033[33;1m"  # Yellow
+G = "\033[32m"    # Green
+R = "\033[31m"    # Red
+O = "\033[39;1m"  # Cya
+B = "\033[34m"    # Blue
+def titulo(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(1. / 500)
+titulo(Y+"""
+ __  __           _                                      _
+|  \/  | __ _  __| | __ _ _ __   _ __ ___  _ __ ___   __| |
+| |\/| |/ _` |/ _` |/ _` | '__| | '_ ` _ \| '_ ` _ \ / _` |
+| |  | | (_| | (_| | (_| | |    | | | | | | | | | | | (_| |
+|_|  |_|\__,_|\__,_|\__,_|_|    |_| |_| |_|_| |_| |_|\__,_|
+
+                                   _
+ _ __   __ _ _   _  __ _ _ __ ___ | |__   __ _ _ __
+| '_ \ / _` | | | |/ _` | '_ ` _ \| '_ \ / _` | '__|
+| |_) | (_| | |_| | (_| | | | | | | |_) | (_| | |
+| .__/ \__,_|\__, |\__,_|_| |_| |_|_.__/ \__,_|_|
+|_|          |___/
+"""); print(R+"Filtering madar mmd payambar"+B+"\nType [ cd madaresh ]"); x = input(G+"\n==>")
+if x == "cd madaresh":
+	titulo(O+"https://sex.payambar.hack.ba.madar.mamad.payambar.dxprit.filteing.rubika")
